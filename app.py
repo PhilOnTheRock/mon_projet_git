fb164da6f84ff79aa679a78ec3d3f3c657c42fa0
PhilOnTheRock <phil.heyraud@gmail.com>
2025-07-08 17:08:11 +0200

    rajout de la fonction Dis Bonjour

--- app.py
+++ app.py
@@ -2,4 +2,7 @@ print('Projet Git local OK')
 # Petit test visuel dans Git pour voir
 #je crois que je commence a comprendre
 # nouvelle ligne de suivie
+def dire_bonjour(nom):
+    print(f"Bonjour {nom} !")
 
+dire_bonjour("Philippe")   
